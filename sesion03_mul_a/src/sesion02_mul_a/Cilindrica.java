8c3c18a431a6bf65b0ada0e55c1120b334cfb1dd
448juls <est.karenj.delgado@unimilitar.edu.co>
2021-08-09 10:38:44 -0500

    Signed-off-by: 448juls <est.karenj.delgado@unimilitar.edu.co>

--- sesion03_mul_a/src/sesion02_mul_a/Cilindrica.java
+++ sesion03_mul_a/src/sesion02_mul_a/Cilindrica.java
@@ -1,5 +1,44 @@
 package sesion02_mul_a;
 
 public class Cilindrica {
+    private float radio;
+    private float angulo;
+    private float z;
+
+    public Cilindrica() {
+    }
+    
+    public Cilindrica(float radio, float angulo, float z) {
+        this.radio = radio;
+        this.angulo = angulo;
+        this.z = z;
+    }
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    public float getAngulo() {
+        return angulo;
+    }
+
+    public void setAngulo(float angulo) {
+        this.angulo = angulo;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float longitud) {
+        this.z = z;
+    }
+    
+    
+    
     
 }
--- sesion03_mul_a/src/sesion02_mul_a/Esferica.java
+++ sesion03_mul_a/src/sesion02_mul_a/Esferica.java
@@ -1,9 +1,50 @@
 package sesion02_mul_a;
 
-/**
- *
- * @author FAMILIA DELGADO
- */
 public class Esferica {
+    private float radioS;
+    private float angulox;
+    private float anguloz;
+
+    public Esferica() {
+   
+    }
+    
+    public Esferica(float radioS, float angulox, float anguloz) {
+        this.radioS = radioS;
+        this.angulox = (float)Math.toRadians(angulox);
+        this.anguloz = (float)Math.toRadians(anguloz);
+    }
+    
+    public Esferica cilincrica_esferica(float radio, float angulo, float z){
+        float p=(float)Math.sqrt(Math.pow(z, 2)+Math.pow(radio, 2));
+        return new Esferica(p, angulo,(float)Math.toDegrees(Math.acos(z/p)));
+    }
+
+    public float getRadioS() {
+        return radioS;
+    }
+
+    public void setRadioS(float radioS) {
+        this.radioS = radioS;
+    }
+
+    public float getAngulox() {
+        return angulox;
+    }
+
+    public void setAngulox(float angulox) {
+        this.angulox = angulox;
+    }
+
+    public float getAnguloz() {
+        return anguloz;
+    }
+
+    public void setAnguloz(float anguloz) {
+        this.anguloz = anguloz;
+    }
+    
+    
+    
     
 }
